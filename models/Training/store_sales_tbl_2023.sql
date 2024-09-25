18c45f696d07341ad9f2b68115e82062609bdaaf
srinivas r <geetharomala@gmail.com>
2024-09-25 08:32:07 +0000

    commit

--- models/Training/store_sales_tbl_2023.sql
+++ models/Training/store_sales_tbl_2023.sql
@@ -4,6 +4,6 @@
     )
 }}
 with store_sales_tbl_2023 as(
-select SS_STORE_SK,SS_QUANTITY,SS_SALES_PRICE,SS_NET_PROFIT from Prod.store_sales_2023
+select SS_STORE_SK,SS_QUANTITY,SS_SALES_PRICE,SS_NET_PROFIT from {{ source('test_prod', 'Store_Sales_2023') }}
 )
 select * from store_sales_tbl_2023
--- models/Training/store_tbl_2023.sql
+++ models/Training/store_tbl_2023.sql
@@ -4,6 +4,6 @@
     )
 }}
 with store_tbl_2023 as (
-select S_STORE_SK, S_STORE_ID, S_STORE_NAME, S_COUNTY from Prod.Store_2023
+select S_STORE_SK, S_STORE_ID, S_STORE_NAME, S_COUNTY from {{ source('test_prod', 'Store_2023') }}
 )
 select * from store_tbl_2023
